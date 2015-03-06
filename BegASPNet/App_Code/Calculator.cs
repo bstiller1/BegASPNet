108a317f42bb1b8ec085f11b230135d222d061cf
Blake Stiller <stiller.blake@gmail.com>
2015-03-06 10:04:32 -0500

    BegASPNet
    Chapter 5 Complete with Calculator.dll

---
+++ BegASPNet/App_Code/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Wrox.Samples
+{
+    public class Calculator
+    {
+        /// <summary>
+        /// This will Add the values that the user enters
+        /// </summary>
+        /// <param name="a">The value entered in ValueBox1</param>
+        /// <param name="b">The value entered in ValueBox2</param>
+        /// <returns>This method return a double value as a result from addition</returns>
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        public double Divide(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
